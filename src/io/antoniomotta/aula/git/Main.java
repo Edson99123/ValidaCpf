5dcf0b960ce379f01e0cbdafe66a95790fe884f7
EdsonDev <edsonsales99@gmail.com>
2021-11-07 11:51:23 -0500

    Criando o projeto de verificação de CPF

---
+++ src/io/antoniomotta/aula/git/Main.java
@@ -0,0 +1,16 @@
+package io.antoniomotta.aula.git;
+
+/**
+ *Classe principal do projeto 
+ * */
+
+public class Main {
+	
+
+public static void main(String[] args) {
+		
+		System.out.println("Olá mundo");
+	}
+
+
+}
